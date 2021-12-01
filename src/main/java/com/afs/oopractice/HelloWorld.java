7a7c694ba93986ce6f3c6e994e88c0d2d1065335
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-01 16:31:59 +0800

    [OOprogramming][Anna]feat:truck.speedup

--- src/main/java/com/afs/oopractice/HelloWorld.java
+++ src/main/java/com/afs/oopractice/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         Car car = new Car("Car",30);
         car.speedup();
+
+        Truck truck = new Truck("Truck", 10);
+        truck.speedup();
     }
 }
